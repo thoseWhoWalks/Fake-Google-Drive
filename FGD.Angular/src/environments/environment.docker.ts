5b6e5f8ae939d7b417f6a988d4ddb791b6164aeb
VladyslavYakymenkoCom <vladyslav.yakymenko.ihorovych@gmail.com>
2023-04-25 21:59:32 +0300

    env (front-end, configuration): development, docker add, apiUrl, signalRUrl as variables add

---
+++ FGD.Angular/src/environments/environment.docker.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: `http://172.20.129.2:5000/api/`,
+    signalRUrl: `http://172.20.128.2:5001/`
+}
--- FGD.Angular/src/environments/environment.prod.ts
+++ FGD.Angular/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: `http://fakegoogledrive.eastus.cloudapp.azure.com:5000/api/`,
+  signalRUrl: `http://fakegoogledrive.eastus.cloudapp.azure.com:5001/`
 };
